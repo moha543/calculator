f93978ce2bc719094f8118d3619c2aceaa584569
ABUKAR HUSSEIN <70592156+moha543@users.noreply.github.com>
2022-11-20 12:58:07 +0300

    Add files via upload
    first commit

---
+++ calcula.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+int add, mult, div, sub;
+add = -2;
+printf("%d\n", add);
+
+mult = -11;
+printf ("%d\n", 2 * mult);
+
+div = 3;
+printf ("%d\n", 21 / div);
+
+sub = 1;
+printf ("%d\n", 22 - sub);
+
+return 0;
+
+}
